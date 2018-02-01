d3149a8c4f25f49d1bd2d01001529b685b4d9277
Jakub Slawecki <Jake@MacBook-Pro-Jakub.local>
2018-02-01 16:11:29 +0100

    NSFetchedResultsController

---
+++ ListItApp/View/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  ListItApp
+//
+//  Created by Jakub Slawecki on 01.02.2018.
+//  Copyright © 2018 Jakub Slawecki. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
